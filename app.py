e77dbc33eb9f045a98cb09630c851c69471565a5
bathirenathan <bathirenathan@nmsworks.co.in>
2020-06-17 13:06:10 +0530

    flask server cereated

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+app.config["DEBUG"] = False
+
+
+@app.route('/', methods=['GET'])
+def home():
+    print(request.args)
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+app.run()
