97820e48d8687db341de0d57e76dac6ff37b856b
bathirenathan <bathirenathan@bathirenathan.nmsworks.co.in>
2020-06-26 12:46:45 +0530

    update notification table for package name

--- DataHandler.py
+++ DataHandler.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate('/home/admin1/Documents/personal-assistant-e59c7-2ad21b6afa8b.json')
+cred = credentials.Certificate('/home/bathirenathan/Downloads/personal-assistant-e59c7-0b71f1cea1d2.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
@@ -12,5 +12,23 @@ def getAllTasks():
     for doc in docs:
         print(f'{doc.id} => {doc.to_dict()}')
 
+def updatePackage():
+    config = db.collection("Configurations").document("PackageToNameMap")
+    doc = config.get()
+    if doc.exists:
+        pack_to_name = doc.to_dict()
+        print(f'Document data: {pack_to_name}')
+        for key, value in pack_to_name.items():
+            print(f'{key}  && {value}')
+            update_notification(key, value)
 
-getAllTasks();
+
+def update_notification(key, value):
+    print(key)
+    notif = db.collection("Notifications").where('packageName', '==', key).stream()
+    for doc in notif:
+        print(f'{doc.id} =>')
+        db.collection("Notifications").document(doc.id).set({u'packageName': value}, merge=True)
+
+
+updatePackage();
--- app.py
+++ app.py
@@ -9,4 +9,9 @@ def home():
     print(request.args)
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
+@app.route('/NotifMapper', methods=['GET'])
+def home():
+    print(request.args)
+    return "success"
+
 app.run()
